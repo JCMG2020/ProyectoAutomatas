03f63a68895052c44db50031e023fd1eb4b72ec2
Jose Carlos <jose.mazariegos@sistemasjole.com>
2020-09-06 13:14:22 -0600

    Carga Inicial

---
+++ ProyectoAutomatas/ProyectoAutomatas/FormularioAutomata.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FormularioAutomata.aspx.cs" Inherits="ProyectoAutomatas.FormularioAutomata" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TxtCadena" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnEvaluaCadena" Text="Evaluar Cadena" OnClick="BtnEvaluaCadena_Click" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProyectoAutomatas/ProyectoAutomatas/FormularioAutomata.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoAutomatas
+{
+    public partial class FormularioAutomata : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnEvaluaCadena_Click(object sender, EventArgs e)
+        {
+            String Cadena = "";
+            Cadena = TxtCadena.Text;
+       
+            char[] charArr = Cadena.ToCharArray();
+            foreach (char ch in charArr)
+            {
+                Console.WriteLine(ch);
+            }
+
+
+        }
+    }
+}
